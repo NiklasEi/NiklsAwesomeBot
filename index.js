47503bca2d23b9bfa4c54bfc4b3429b1782f2366
Niklas Eicker <nikle1@gmx.de>
2018-11-03 01:42:25 +0100

    correctly answer query callback and add command /games

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const Promise = require('bluebird');
 Promise.config({
     cancellation: true
 });
-var TelegramBot = require( "node-telegram-bot-api" );
-var token = process.env.BOT_TOKEN;
-var nowUrl = process.env.NOW_URL;
+let TelegramBot = require( "node-telegram-bot-api" );
+let token = process.env.BOT_TOKEN;
+let nowUrl = process.env.NOW_URL;
+let gamesBaseUrl = process.env.GAMES_BASE_URL;
 
 const options = {
     webHook: {
@@ -12,60 +13,60 @@ const options = {
     }
 };
 
-var bot = new TelegramBot( token, options );
+const bot = new TelegramBot( token, options );
 bot.setWebHook(`${nowUrl}/bot${token}`);
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-});
+// games:
+const knownGames = {
+    "minesweeper": new Game("minesweeper", "Minesweeper")
+};
 
 bot.onText( /\/play (.+)/, function( msg, match ) {
-    var fromId = msg.from.id;
-    switch( match[1].toLowerCase() ) {
-        case "minesweeper":
-            bot.sendGame(
-                fromId,
-                "minesweeper",
-                {
-                    reply_markup: JSON.stringify({
-                        inline_keyboard: [
-                            [ { text: "Play", callback_game: JSON.stringify( { game_short_name: "TestGame" } ) } ],
-                            [ { text: "Share", url: "https://telegram.me/NiklsAwesomeBot?game=minesweeper" } ]
-                        ]
-                    })
-                }
-            );
-            break;
-        default:
-            bot.sendMessage( fromId, "Sorry " + msg.from.first_name + ", but this game doesn’t exist (yet)..." );
+    let fromId = msg.from.id;
+    if (knownGames.hasOwnProperty(match[1].toLowerCase())) {
+        bot.sendGame(
+            fromId,
+            match[1].toLowerCase(),
+            {
+                reply_markup: JSON.stringify({
+                    inline_keyboard: [
+                        [ { text: "Play", callback_game: JSON.stringify( { game_short_name: match[1].toLowerCase() } ) } ],
+                        [ { text: "Share", url: "https://telegram.me/NiklsAwesomeBot?game=" + match[1].toLowerCase() } ]
+                    ]
+                })
+            }
+        );
+    } else {
+        bot.sendMessage( fromId, "Sorry " + msg.from.first_name + ", but this game doesn't exist (yet)...\nRun /games to see all games" );
     }
+});
+
+bot.onText( /\/games/, function( msg, match ) {
+    let fromId = msg.from.id;
+    let response = "You can play:";
+    for (let key in knownGames) {
+        if (!knownGames.hasOwnProperty(key)) continue;
+        response += "\n" + knownGames[key].game_short_name;
+    }
+    bot.sendMessage( fromId, response );
 });
 
 bot.on( "callback_query", function( cq ) {
     if ( cq.game_short_name ) {
-        switch( cq.game_short_name ) {
-            case "minesweeper":
-                bot.answerCallbackQuery( cq.id, undefined, false, { url: "https://html5-minesweeper-gynkmlngaa.now.sh" } );
-                return;
+        if (knownGames.hasOwnProperty(cq.game_short_name.toLowerCase())) {
+            console.log("answer query with: " + knownGames[cq.game_short_name.toLowerCase()].url);
+            bot.answerCallbackQuery( cq.id, { url: knownGames[cq.game_short_name.toLowerCase()].url });
+        } else {
+            bot.answerCallbackQuery( cq.id, "Sorry, '" + cq.game_short_name + "' is not available at the moment.", true );
         }
-        bot.answerCallbackQuery( cq.id, "Sorry, '" + cq.game_short_name + "' is not available.", true );
     }
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
-});
+function Game(game_short_name, name) {
+    this.game_short_name = game_short_name;
+    this.name = name;
+    this.url = "https://" + game_short_name + "." + gamesBaseUrl;
+    this.changeUrl = function (newUrl) {
+        this.url = newUrl;
+    }
+}
