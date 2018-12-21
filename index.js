45ca4663885d07dbe25638aadc9ae6ef26f1b41e
Niklas Eicker <nikle1@gmx.de>
2018-12-21 15:33:30 +0100

    add custom keyboard on /games and cleanup

--- index.js
+++ index.js
@@ -28,6 +28,38 @@ const knownGames = {
     "sudoku": new Game("sudoku", "Sudoku")
 };
 
+
+// define internal bot constants:
+const games_inline_keyboard_name_prefix = "🎮";
+const games_inline_keyboard = {
+    keyboard: [],
+    one_time_keyboard: true
+};
+const game_names_to_id = {};
+
+// loop through all games and prepare game specific objects
+let even = true;
+for(const gameID in knownGames) {
+    if (!knownGames.hasOwnProperty(gameID)) continue;
+    even = !even;
+    let game = knownGames[gameID];
+
+    // fill inline keyboard for games
+    if (!even) {
+        games_inline_keyboard.keyboard.push([{ text: games_inline_keyboard_name_prefix + " " + game.name }]);
+    } else {
+        games_inline_keyboard.keyboard[games_inline_keyboard.keyboard.length - 1]
+            .push({ text: games_inline_keyboard_name_prefix + " " + game.name });
+    }
+    // prepare mapping from game names to IDs
+    //   with this not only the IDs have to be unique, but also the names. ToDo: so why use IDs?
+    game_names_to_id[game.name] = game.game_short_name;
+}
+
+/*
+    bot internals
+ */
+
 bot.onText( /\/play (.+)/, function( msg, match ) {
     let fromId = msg.from.id;
     let lowerCaseMatch = match[1].toLowerCase();
@@ -43,9 +75,9 @@ bot.onText( /\/play (.+)/, function( msg, match ) {
                     ]
                 })
             }
-        );
+        ).then();
     } else {
-        bot.sendMessage( fromId, "Sorry " + msg.from.first_name + ", but this game doesn't exist (yet)...\nRun /games to see all games" );
+        bot.sendMessage( fromId, "Sorry " + msg.from.first_name + ", but this game doesn't exist (yet)...\nRun /games to see all games" ).then();
     }
 });
 
@@ -54,27 +86,43 @@ bot.onText( /\/start/, function( msg ) {
     let fromId = msg.from.id;
     let user = msg.from.first_name;
     let response = "Hi there " + user + ",\n";
-    response += "Run /games for a list of available games.";
-    bot.sendMessage( fromId, response );
+    response += "Run /games to see all available games";
+    bot.sendMessage( fromId, response ).then();
+});
+
+bot.onText( /🎮 (.+)/, function( msg, match ) {
+    if(!game_names_to_id.hasOwnProperty(match[1])) return;
+    let game_short_name = game_names_to_id[match[1]];
+    bot.sendGame(
+        msg.from.id,
+        game_short_name,
+        {
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                    [ { text: "Play 🎮", callback_game: JSON.stringify( { game_short_name: game_short_name } ) } ],
+                    [ { text: "Share 🗣", url: "https://telegram.me/" + botName + "?game=" + game_short_name } ]
+                ]
+            })
+        }
+    ).then();
 });
 
 bot.onText( /\/games/, function( msg ) {
     let fromId = msg.from.id;
-    let response = "You can play:";
-    for (let key in knownGames) {
-        if (!knownGames.hasOwnProperty(key)) continue;
-        response += "\n" + knownGames[key].name;
-    }
-    bot.sendMessage( fromId, response );
+    bot.sendMessage( fromId, "Which game would you like to play?",
+        {
+            reply_markup: JSON.stringify(games_inline_keyboard)
+        }
+    ).then();
 });
 
 bot.on( "callback_query", function( cq ) {
     if ( cq.game_short_name ) {
         if (knownGames.hasOwnProperty(cq.game_short_name.toLowerCase())) {
             console.log("answer query with: " + knownGames[cq.game_short_name.toLowerCase()].url);
-            bot.answerCallbackQuery( cq.id, { url: knownGames[cq.game_short_name.toLowerCase()].url });
+            bot.answerCallbackQuery( cq.id, { url: knownGames[cq.game_short_name.toLowerCase()].url }).then();
         } else {
-            bot.answerCallbackQuery( cq.id, "Sorry, '" + cq.game_short_name + "' is not available at the moment.", true );
+            bot.answerCallbackQuery( cq.id, "Sorry, '" + cq.game_short_name + "' is not available at the moment.", true ).then();
         }
     }
 });
@@ -99,6 +147,7 @@ bot.on( "inline_query", function(iq) {
     });
 });
 
+// game constructor
 function Game(game_short_name, name) {
     this.game_short_name = game_short_name;
     this.name = name;
