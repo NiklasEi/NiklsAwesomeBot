af704aa376f3aff5961cb440cf03303b8202f3b5
Niklas Eicker <nikle1@gmx.de>
2018-12-28 20:01:57 +0100

    overwrite default web hook listener to add own api endpoints

--- index.js
+++ index.js
@@ -8,6 +8,40 @@ Promise.config({
 process.env.NTBA_FIX_319 = true;
 
 let TelegramBot = require( "node-telegram-bot-api" );
+const debug = require('debug')('node-telegram-bot-api');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const bl = require('bl');
+// overwrite web hook to add my own api...
+require("node-telegram-bot-api/src/telegramWebHook").prototype._requestListener = function(req, res) {
+    debug('WebHook request URL: %s', req.url);
+    debug('WebHook request headers: %j', req.headers);
+
+    if (req.url.indexOf(this.bot.token) !== -1) {
+        if (req.method !== 'POST') {
+            debug('WebHook request isn\'t a POST');
+            res.statusCode = 418; // I'm a teabot!
+            res.end();
+        } else {
+            req
+                .pipe(bl(this._parseBody))
+                .on('finish', () => res.end('OK'));
+        }
+    } else if (req.url.indexOf("test") !== -1) {
+        res.statusCode = 200;
+        res.end('OK');
+    } else if (this._healthRegex.test(req.url)) {
+        debug('WebHook health check passed');
+        res.statusCode = 200;
+        res.end('OK');
+    } else {
+        debug('WebHook request unauthorized');
+        res.statusCode = 401;
+        res.end();
+    }
+};
+
 let token = process.env.BOT_TOKEN;
 let nowUrl = process.env.NOW_URL;
 let gamesBaseUrl = process.env.BASE_URL;
